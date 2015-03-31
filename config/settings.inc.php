0192d2c302c67c22872523eeed6f8172a567837d
Louis Serre <louis.serre@gmail.com>
2015-03-31 14:23:20 +0200

    Adds db config file

---
+++ config/settings.inc.php
@@ -0,0 +1,16 @@
+<?php
+define('_DB_SERVER_', 'localhost');
+define('_DB_NAME_', 'prestashop');
+define('_DB_USER_', 'root');
+define('_DB_PASSWD_', 'root');
+define('_DB_PREFIX_', 'ps_');
+define('_MYSQL_ENGINE_', 'InnoDB');
+define('_PS_CACHING_SYSTEM_', 'CacheMemcache');
+define('_PS_CACHE_ENABLED_', '0');
+define('_COOKIE_KEY_', 'AV8cfCNkKaKmBJgag8QGRxZZNsy32NFrj6VRBp5oo5iOOH4FCYN0PCEe');
+define('_COOKIE_IV_', 'QzcOedpD');
+define('_PS_CREATION_DATE_', '2015-03-26');
+if (!defined('_PS_VERSION_'))
+	define('_PS_VERSION_', '1.6.0.14');
+define('_RIJNDAEL_KEY_', 'uhDYTYASFoJn9Zi779mykw3aL6GSrfjy');
+define('_RIJNDAEL_IV_', 'DbHIleHOCCUjiu7+0BPLSA==');
